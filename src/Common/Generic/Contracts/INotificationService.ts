aa3e139d8c0c8e7f336235123302161f01c551a4
thewitcher7667 <aymanwilliam604@gmail.com>
2025-03-09 22:17:48 +0200

    Add verify email template

--- src/Common/Generic/Contracts/INotificationService.ts
+++ src/Common/Generic/Contracts/INotificationService.ts
@@ -2,6 +2,8 @@ export interface INotification
 {
     SendResetPass(toEmail:string,code:number):Promise<void>
 
+    SendVerifyLink(toEmail:string,token:string):Promise<void>
+
     SentNotification()
 
     SendEmailAndNotification()
--- src/Infrastructure/Notification/Services/Notification.service.ts
+++ src/Infrastructure/Notification/Services/Notification.service.ts
@@ -2,12 +2,26 @@ import { Injectable, Provider, Scope } from "@nestjs/common";
 import { INotification } from "src/Common/Generic/Contracts/INotificationService";
 import { EmailService } from "./Email.service";
 import { IEmailDto } from "../Dtos/IEmail.dto";
+import { ConfigService } from "@nestjs/config";
 
 @Injectable({scope:Scope.REQUEST})
 export class NotificationService implements INotification
 {
-    constructor(private readonly emailService:EmailService)
+    constructor(private readonly emailService:EmailService,private readonly configService:ConfigService)
     {}
+
+    async SendVerifyLink(toEmail: string, token: string): Promise<void> {
+        
+        await this.emailService.SendEmail({
+            Subject:"Verification",
+            HtmlTemplateFilePath:"verification",
+            To:toEmail,
+            Message:"Verify your identity",
+            Context:{
+                Link:`${this.GetFrontendLink()}/users/verify?Email=${toEmail}&Token=${token}`
+            }
+        });
+    }
 
     async SendResetPass(toEmail:string,code:number): Promise<void> {
 
@@ -30,6 +44,12 @@ export class NotificationService implements INotification
         throw new Error("Method not implemented.");
     }
     
+    private GetFrontendLink():string
+    {
+        return `${this.configService.getOrThrow<string>("FRONTEND_PROTOCOL")}://`+
+                `${this.configService.getOrThrow<string>("FRONTEND_DOMAIN")}:` +
+                `${this.configService.getOrThrow<string>("FRONTEND_PORT")}`
+    }
 }
 
 export const NotificationServiceProvider:Provider = {
