0dc01bcc11ea4b92cc17097eddd3c059d6a8c8bd
thewitcher7667 <aymanwilliam604@gmail.com>
2025-03-18 19:48:53 +0200

    Add File Types
    - CommunityLogoFile CommunityLogoFileOptions
    
    - CommunityImagesFile
    CommunityImagesFileOptions

---
+++ src/Common/FileUpload/FileTypes/CommunityLogo.file.ts
@@ -0,0 +1,14 @@
+import { IFile } from "./IFile";
+import { IFileTypes } from "./Types/IFileTypes";
+import { ImageFileType } from "./Types/Image.filetypes";
+
+class CommunityLogoFile implements IFile
+{
+    MaxSize: number = 1024 * 1024 * 5;
+
+    Dest: string = "\\communities\\logo\\";
+
+    FileType: IFileTypes = new ImageFileType();
+}
+
+export const CommunityLogoFileOptions =  new CommunityLogoFile();
