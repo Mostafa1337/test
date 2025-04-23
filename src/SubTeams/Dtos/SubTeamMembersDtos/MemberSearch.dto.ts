d244a87961755ce528a87b1f4a7b61faeb1b6f04
thewitcher7667 <aymanwilliam604@gmail.com>
2025-04-23 21:09:29 +0200

    Update MemberSearch.dto.ts

--- src/SubTeams/Dtos/SubTeamMembersDtos/MemberSearch.dto.ts
+++ src/SubTeams/Dtos/SubTeamMembersDtos/MemberSearch.dto.ts
@@ -23,19 +23,19 @@ export class MemberSearchDto extends Pagination
     UserEmail?:string = null
 
     @IsIn([true,false])
-    @ApiProperty({required:false,default:null,description:"filter members who is accepted in the sub team ( this means the member has a join date != null)"})
+    @ApiProperty({required:false,default:null,description:"filter members based on who is accepted in the sub team ( this means the member has a join date != null)"})
     @IsOptional()
     @Transform(({value})=> value === "true" ? true : false)
     IsAccepted?:boolean  = null
 
     @IsIn([true,false])
-    @ApiProperty({required:false,default:false,description:"filter members who left/kicked the sub team ( this means the member has a leave date != null)"})
+    @ApiProperty({required:false,default:false,description:"filter members based on whether they left the sub team or has been kicked ( this means the member has a leave date != null)"})
     @IsOptional()
     @Transform(({value})=> value === "true" ? true : false)
     IsLeft?:boolean  = false
 
     @IsIn([true,false])
-    @ApiProperty({required:false,default:null,description:"filter members who is sub team head and who is not"})
+    @ApiProperty({required:false,default:null,description:"Filter members based on whether they are sub-team heads or not"})
     @IsOptional()
     @Transform(({value})=> value === "true" ? true : false)
     IsHead?:boolean = null
